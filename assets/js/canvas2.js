8c6d409a405113052261319c45b644195ee8787d
intermorph <intermorphmusic@gmail.com>
2023-12-30 15:14:17 +0100

    changed whoami

---
+++ assets/js/canvas2.js
@@ -0,0 +1,293 @@
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+// cool page to see if you are on a mobile or not: http://detectmobilebrowsers.com
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+window.mobileCheck = function() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+var canvas = document.getElementById("canvas2");
+var context = canvas.getContext("2d");
+var xVal = 0;
+
+canvas.width = window.innerWidth;
+if (mobileCheck() == false){
+	canvas.height = window.innerHeight / 2;
+} else {
+	canvas.height = window.innerHeight / 4;
+}
+
+if (mobileCheck() == false){
+
+	var initMinSize = 2;
+	var initMaxSize = 8;
+	var initNumCircles = 225;
+	var initMinSpeed = -1;
+	var initMaxSpeed = 1;
+	var initHeight = window.innerHeight;
+	var state = 0;
+
+} else {
+	
+	var initMinSize = 1;
+	var initMaxSize = 4;
+	var initNumCircles = 90;
+	var initMinSpeed = -1;
+	var initMaxSpeed = 1;
+	var initHeight = window.innerHeight;
+	var state = 0;
+	
+}
+
+var circles = [],
+    
+    // SETTINGS 
+    opacity = 1,                                      // the opacity of the circles 0 to 1
+    colors = ['rgba(255, 255, 255,' + opacity + ')'],
+    minSize = initMinSize,                                        // the minimum size of the circles in px
+    maxSize = initMaxSize,                                       // the maximum size of the circles in px
+    numCircles = initNumCircles,                                   // the number of circles
+    minSpeed = initMinSpeed,                                     // the minimum speed, recommended: -maxspeed
+    maxSpeed = initMaxSpeed,                                    // the maximum speed of the circles
+    expandState = true;                                      // the direction of expansion
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+function buildArray() {
+    'use strict';
+    
+    for (var i =0; i < numCircles ; i++){
+        var color = Math.floor(Math.random() * (colors.length - 1 + 1)) + 1,
+            left = Math.floor(Math.random() * (canvas.width - 0 + 1)) + 0,
+            top = Math.floor(Math.random() * (canvas.height - 0 + 1)) + 0,
+            size = Math.floor(Math.random() * (maxSize - minSize + 1)) + minSize,
+            leftSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10,
+            topSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10,
+            expandState = expandState;
+           
+            while(leftSpeed == 0 || topSpeed == 0){
+                leftSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10,
+                topSpeed = (Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed)/10;
+            }
+        var circle = {color:color, left:left, top:top, size:size, leftSpeed:leftSpeed, topSpeed:topSpeed, expandState:expandState };
+        circles.push(circle);
+    }
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+function build(){
+    'use strict';
+    
+    for(var h = 0; h < circles.length; h++){
+        var curCircle = circles[h];
+        context.fillStyle = colors[curCircle.color-1];
+        context.beginPath();
+        if(curCircle.left > canvas.width+curCircle.size){
+            curCircle.left = 0-curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+        }else if(curCircle.left < 0-curCircle.size){
+            curCircle.left = canvas.width+curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+        }else{
+            curCircle.left = curCircle.left+curCircle.leftSpeed;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false); 
+        }
+        
+        if(curCircle.top > canvas.height+curCircle.size){
+            curCircle.top = 0-curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+
+        }else if(curCircle.top < 0-curCircle.size){
+            curCircle.top = canvas.height+curCircle.size;
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false);
+        }else{
+            curCircle.top = curCircle.top+curCircle.topSpeed;
+            if(curCircle.size != maxSize && curCircle.size != minSize && curCircle.expandState == false){
+              curCircle.size = curCircle.size-0.1;
+            }
+            else if(curCircle.size != maxSize && curCircle.size != minSize && curCircle.expandState == true){
+              curCircle.size = curCircle.size+0.1;
+            }
+            else if(curCircle.size == maxSize && curCircle.expandState == true){
+              curCircle.expandState = false;
+              curCircle.size = curCircle.size-0.1;
+            }
+            else if(curCircle.size == minSize && curCircle.expandState == false){
+              curCircle.expandState = true;
+              curCircle.size = curCircle.size+0.1;
+            }
+            context.arc(curCircle.left, curCircle.top, curCircle.size, 0, 2 * Math.PI, false); 
+        }
+        
+        context.closePath();
+        context.fill();
+        context.ellipse;
+    }
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+window.requestAnimFrame = (function (callback) {
+    'use strict';
+	
+	var reqAnim = window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (callback) {
+			window.setTimeout(callback, 1000/60);
+		};
+	
+    return reqAnim;
+	
+})();
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+function animate() {
+    'use strict';
+    var canvas = document.getElementById("canvas2"),
+        context = canvas.getContext("2d");
+
+    // clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw the next frame
+    xVal++;
+    build();
+
+    //console.log("Prep: animate ==> requestAnimFrame");
+    // request a new frame
+    requestAnimFrame(function () {
+        animate();
+    });
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+window.onload = function () {
+	
+    'use strict';
+    buildArray();
+    animate();
+	
+};
+
+/*
+window.onresize = function () {
+	
+	if (mobileCheck() == false){
+	
+		window.cancelAnimationFrame(xVal);
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight / 2;
+		circles = []
+		minSize = Math.round(initMinSize * (window.innerHeight / initHeight));
+		maxSize = Math.round(initMaxSize * (window.innerHeight / initHeight));
+		minSpeed = Math.round(initMinSpeed * (window.innerHeight / initHeight));
+		maxSpeed = Math.round(initMaxSpeed * (window.innerHeight / initHeight));
+		buildArray();
+		animate();
+		
+	}
+	
+};
+*/
+
+canvas.onclick = function() { 
+
+	if (mobileCheck() == false){
+
+		window.cancelAnimationFrame(xVal);
+		circles = []
+		
+		if (state == 0){
+			
+			minSpeed = initMinSpeed - 3;
+			maxSpeed = initMaxSpeed + 3;
+			//numCircles = initNumCircles - 40;
+			state = 1;
+		
+		} else if (state == 1) {
+			
+			minSpeed = initMinSpeed - 6;
+			maxSpeed = initMaxSpeed + 6;
+			//numCircles = initNumCircles - 80;
+			state = 2;
+			
+		} else if (state == 2) {
+			
+			minSpeed = initMinSpeed;
+			maxSpeed = initMaxSpeed;
+			//numCircles = initNumCircles;
+			state = 0;
+			
+		}
+
+		buildArray();
+		animate();
+		
+	}
+
+};
+
+
+canvas.onmouseover = function() {
+	
+	if (mobileCheck() == false){
+	
+		window.cancelAnimationFrame(xVal);
+		circles = []
+		colors = ['rgba(0, 0, 0,' + opacity + ')']
+				 
+		buildArray();
+		animate();
+		
+	}
+	
+};
+
+canvas.onmouseout = function() {
+	
+	if (mobileCheck() == false){
+	
+		window.cancelAnimationFrame(xVal);
+		circles = []
+		colors = ['rgba(255, 255, 255,' + opacity + ')']
+				 
+		buildArray();
+		animate();
+	
+	}
+	
+};
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+// Remove hover on mobiles
+
+if (mobileCheck() == false) {
+	document.body.className += ' hasHover';
+}
+
+console.log(document.body.className);
+
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------*/
